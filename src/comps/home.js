58cd4446415269f4efdd45df4ccba8ccea99f922
moathar <66837928+moathar@users.noreply.github.com>
2022-01-27 17:12:53 +0000

    home page

--- src/comps/home.js
+++ src/comps/home.js
@@ -1,6 +1,13 @@
 // this is a sample home page
 // TODO: layout, header, footer, menubars etc.
-import { Grid, Typography, Item } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+  Button
+} from "@mui/material";
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import { UserStore } from "../../store/userStore";
@@ -16,12 +23,42 @@ export default function Home() {
       <Grid container>
         <OurAppBar />
       </Grid>
-      <Grid>
+
+      <Grid item>
         <Typography>
-          Wohoo <span style={{ color: "green" }}>{userName}</span> you have
-          landed on the home page, now select what you want to do.
+          Wohoo <span style={{ color: "green" }}>{userName}</span>, the breaking
+          news is that bannana milkshakes are the thing.
         </Typography>
       </Grid>
+
+      <Grid item xs={12} sm={6}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography>
+              Check out the latest fruitulous recipes here to fill the life of
+              your customers with joy!
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Recipes</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography>
+              Connect with the community and meet Dumbo's extended family of
+              fruit farmers
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Connect</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
       <Outlet />
     </Grid>
   );
