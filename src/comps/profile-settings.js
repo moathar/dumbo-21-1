250b5bf0d105a12731851b0c57760bffead840a0
moathar <66837928+moathar@users.noreply.github.com>
2022-02-14 16:50:23 +0000

    settings proto

--- src/comps/profile-settings.js
+++ src/comps/profile-settings.js
@@ -5,7 +5,15 @@ import {
   CardActions,
   CardContent,
   Button,
-  CardHeader
+  CardHeader,
+  Switch,
+  Select,
+  MenuItem,
+  inputLabelClasses,
+  InputLabel,
+  FormGroup,
+  FormControl,
+  FormControlLabel
 } from "@mui/material";
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
@@ -14,7 +22,17 @@ import OurAppBar from "./ourAppBar";
 
 export default function Settings() {
   const userName = UserStore.useState((s) => s.name);
-  const theme = UserStore.useState((s) => s.currentTheme);
+  const theme = UserStore.useState((s) => s.currentTheme); // tidy this up
+  const [isDarkTheme, setDarkTheme] = React.useState(false);
+  const [font, setFont] = React.useState("Roboto");
+
+  const handleThemeChange = (event) => {
+    setDarkTheme(event.target.checked);
+  };
+
+  const handleFontChange = (event) => {
+    setFont(event.target.value);
+  };
 
   return (
     // overall layout
@@ -22,8 +40,38 @@ export default function Settings() {
     <Grid container spacing={1}>
       <Grid item xs={12} sm={6}>
         <Card sx={{ minWidth: 275 }}>
-          <CardHeader title="Theme" />
-          <CardContent> </CardContent>
+          <CardHeader subheader="Typography" />
+          <CardContent>
+            <FormControl fullWidth>
+              <InputLabel id="font-select">Font</InputLabel>
+              <Select
+                id="font-select"
+                value={font}
+                label="Font"
+                onChange={handleFontChange}
+              >
+                <MenuItem value={"Roboto"}>Roboto</MenuItem>
+                <MenuItem value={"Times Roman"}>Times Roman</MenuItem>
+                <MenuItem value={"Verdana"}>Verdana</MenuItem>
+              </Select>
+            </FormControl>
+          </CardContent>
+          <CardActions></CardActions>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardHeader subheader="Theme" />
+          <CardContent>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch checked={isDarkTheme} onChange={handleThemeChange} />
+                }
+                label="Dark Theme"
+              />
+            </FormGroup>
+          </CardContent>
           <CardActions></CardActions>
         </Card>
       </Grid>
